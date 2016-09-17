9ead6fedfe4b7045bdfbbb4b47e510fe8ab74972
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-09-17 23:29:58 +0200

    Various minor changes. Json files are now created in specific tasks which are invoked before the build task.

--- plugin/src/main/groovy/com/github/wrdlbrnft/jsongenerator/JsonGeneratorPlugin.groovy
+++ plugin/src/main/groovy/com/github/wrdlbrnft/jsongenerator/JsonGeneratorPlugin.groovy
@@ -22,30 +22,52 @@ class JsonGeneratorPlugin implements Plugin<Project> {
                 cleanTask = project.task('clean') {}
             }
 
+            def buildTask = project.tasks.findByName('build')
+            if (buildTask == null) {
+                buildTask = project.task('build') {}
+            }
+
             project.extensions.jsonGenerator.each { JsonGeneratorExtension extension ->
                 validateExtension(extension);
-                createCleanTaskForExtension(project, cleanTask, extension);
-                String json = extension.template.text
-                extension.variables.keySet().each { key ->
-                    def value = String.valueOf(extension.variables.get(key))
-                    json = json.replace('$[' + key + ']$', value)
-                }
-                extension.output.withWriter { out ->
-                    out.write(json);
-                }
+                def cleanJsonFileTask = createCleanTaskForExtension(project, cleanTask, extension);
+                createCreateTaskForExtension(project, buildTask, cleanJsonFileTask, extension);
             }
         }
     }
 
-    static def createCleanTaskForExtension(Project project, Task cleanTask, JsonGeneratorExtension extension) {
+    static def createCreateTaskForExtension(Project project, Task buildTask, Task cleanJsonFileTask, JsonGeneratorExtension extension) {
         def extensionName = extension.name.substring(0, 1).toUpperCase() + extension.name.substring(1);
-        def taskName = 'clean' + extensionName + 'GeneratedJsonFiles'
-        cleanTask.dependsOn project.task(taskName, type: Delete) {
-            delete extension.output
+        def taskName = 'create' + extensionName + 'GeneratedJsonFile'
+        def createJsonFileTask = project.task(taskName) << {
+            def json = extension.template.text
+            extension.variables.keySet().each { key ->
+                def value = String.valueOf(extension.variables.get(key))
+                json = json.replace('$[' + key + ']$', value)
+            }
+            extension.output.withWriter { out ->
+                out.write(json);
+            }
+            if (!extension.output.exists()) {
+                throw new ProjectConfigurationException('Failed to create json file for ' + extension.name, null)
+            }
+            println extension.name + ' Json File has been created: ' + extension.output
         }
+        createJsonFileTask.dependsOn cleanJsonFileTask
+        buildTask.dependsOn createJsonFileTask
+        createJsonFileTask
     }
 
-    static void validateExtension(JsonGeneratorExtension extension) {
+    static def createCleanTaskForExtension(Project project, Task cleanTask, JsonGeneratorExtension extension) {
+        def extensionName = extension.name.substring(0, 1).toUpperCase() + extension.name.substring(1);
+        def taskName = 'clean' + extensionName + 'GeneratedJsonFile'
+        def cleanJsonFileTask = project.task(taskName, type: Delete) {
+            delete extension.output
+        }
+        cleanTask.dependsOn cleanJsonFileTask
+        cleanJsonFileTask
+    }
+
+    static def validateExtension(JsonGeneratorExtension extension) {
         if (extension.template == null || extension.output == null) {
             throw new ProjectConfigurationException('You need to specify at least a template file and an output file to generate a json', null)
         }
